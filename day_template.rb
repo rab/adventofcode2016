8c620d077875321de94ead663411e7cbcd6fcd03
Rob Biedenharn <Rob@AgileConsultingLLC.com>
2016-12-15 10:28:58 -0500

    Day 15: Timing is Everything (1st & 2nd)

--- day_template.rb
+++ day_template.rb
@@ -3,4 +3,4 @@ require_relative 'input'
 day = __FILE__[/\d+/].to_i(10)
 input = Input.for_day(day)
 
-puts "solving day #{day} from input\n#{input.inspect}"
+puts "solving day #{day} from input:\n#{input}"
